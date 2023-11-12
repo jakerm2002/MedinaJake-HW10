f8b5d8438e7ddf18332e29baae4dbef06688550e
Jake Medina <jake.medina02@gmail.com>
2023-11-11 18:05:19 -0600

    initial commit

---
+++ MedinaJake-HW10/ImageViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageViewCell.swift
+//  MedinaJake-HW10
+//
+//  Created by Jake Medina on 11/11/23.
+//
+
+import UIKit
+
+class ImageViewCell: UICollectionViewCell {
+    
+}
--- MedinaJake-HW10/ViewController.swift
+++ MedinaJake-HW10/ViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let imageViewCellIdentifier = "ImageViewCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        
+        collectionView.collectionViewLayout = layout
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: imageViewCellIdentifier, for: indexPath) as! ImageViewCell
+        
+        return cell
     }
 
 
